ebfc3d76f4391f98bd2c319622acd2d8d5dd4169
jeeno44 <jeep65418@gmail.com>
2023-05-22 19:31:59 +0300

    My first commit on kotlin project

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+import java.time.Clock as cl
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    var mina:Int = 100
+
+    while (mina < 150){
+        println("yes = " + mina.toString())
+        mina += 1
+    }
+
+    println(mina.toInt()::class.java.typeName)
+
+//    for (i in 0..50 step 3)
+//        println(i.toString() + " Порядок")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
